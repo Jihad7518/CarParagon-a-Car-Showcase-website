19576a9abf79f2675affb9bf3ca67b0e5577a6e8
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-02 01:53:20 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,21 @@
 
+import "./globals.css";
+
+import { Footer, NavBar } from "@components";
+
+export const metadata = {
+  title: "CarParagon",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
