49b5b2c5b3e58498d05d62974b07e648b719dd35
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-05 16:02:48 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,3 +12,30 @@ const Hero = () => {
       nextSection.scrollIntoView({ behavior: "smooth" });
     }
   };
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+        Effortless Car Discovery, Booking, and Rental – Fast and Intuitive!
+        </h1>
+
+        <p className="hero__subtitle">
+        Simplify Your Car Rental: Effortless Booking Made Easy!
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-blue-900 text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
