e5712affbadc2d4835dc2ad265e39c2cc5586517
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-05 16:02:17 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,3 +33,20 @@ const Footer = () => (
         ))}
       </div>
     </div>
+
+    <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
+      <p>CarParagon made by <a href="https://www.linkedin.com/in/md-jihad-8685a424a/" className="text-blue-900"> Md. Jihad </a> © All Rights Reserved</p>
+
+      <div className="footer__copyrights-link">
+        <Link href="/" className="text-gray-500">
+          Privacy & Policy
+        </Link>
+        <Link href="/" className="text-gray-500">
+          Terms & Condition
+        </Link>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
