34bfc7760009a562892fe23a4e330f76e4d358cc
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-05 16:02:30 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,1 +1,14 @@
+"use client";
 
+import Image from "next/image";
+
+import { CustomButton } from "@components";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
