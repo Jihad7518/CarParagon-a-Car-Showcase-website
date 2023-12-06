cee159339db4d0b73082016f63ccecd84342e040
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-06 11:12:14 +0600

    Update SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -17,3 +17,34 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
         );
+ return (
+    <div className='search-manufacturer'>
+      <Combobox value={manufacturer} onChange={setManuFacturer}>
+        <div className='relative w-full'>
+          {/* Button for the combobox. Click on the icon to see the complete dropdown */}
+          <Combobox.Button className='absolute top-[14px]'>
+            <Image
+              src='/car-logo.svg'
+              width={20}
+              height={20}
+              className='ml-4'
+              alt='car logo'
+            />
+          </Combobox.Button>
+
+          {/* Input field for searching */}
+          <Combobox.Input
+            className='search-manufacturer__input'
+            displayValue={(item: string) => item}
+            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
+            placeholder='Volkswagen...'
+          />
+
+          {/* Transition for displaying the options */}
+          <Transition
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+          >
