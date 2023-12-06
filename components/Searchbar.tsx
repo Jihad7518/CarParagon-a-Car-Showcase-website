bf6e7ed69456490e25781520aae2c33698ba593b
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-06 11:13:36 +0600

    Update Searchbar.tsx

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -30,7 +30,43 @@ const SearchBar = () => {
     if (manufacturer.trim() === "" && model.trim() === "") {
       return alert("Please provide some input");
     }
+    // Generate the new pathname with the updated search parameters
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
 
+    router.push(newPathname);
+  };
+
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
+    </form>
+  );
+};
     updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
   };
 
